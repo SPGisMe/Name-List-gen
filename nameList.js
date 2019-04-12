612b76e6ad6e85d5d16938f63ddbb2c7cedf9d7a
Sean Gaston <46758834+SPGisMe@users.noreply.github.com>
2019-04-11 17:58:54 -0700

    Update nameList.js

--- nameList.js
+++ nameList.js
@@ -3,8 +3,8 @@ function list(names){ //names as a hash list[{name:john},{name:Dan}]
   
   for (let x of names) {
     names.length == 1 ? nameList = x.name
-    : names.indexOf(x) == names.length - 2 ? nameList += `${x.name} `
-    : names.indexOf(x) == names.length - 1 ? nameList += `& ${x.name}`
+    : names.indexOf(x) == names.length - 2 ? nameList += `${x.name} ` //current and next line format so that last 2
+    : names.indexOf(x) == names.length - 1 ? nameList += `& ${x.name}` //names are formatted as "name & name"
     : nameList += `${x.name}, `;
   }
   return nameList;
